6e1723ffe05ee2eae4a2a1e5a9951db2c8abf6d4
yuktadandeva <153160647+yuktadandeva@users.noreply.github.com>
2024-07-08 23:37:40 +0530

    Delete PIZZA STORE directory

--- PIZZA STORE/scripts/api.client.js
+++
@@ -1,8 +0,0 @@
-
-export function getPizzas(){
-    const URL ="https://raw.githubusercontent.com/yuktadandeva/pizza-api/main/pizza.json";
-
-    const promise = fetch(URL);
-
-    return promise;
-}
