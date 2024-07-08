c942e9d8f39d35d8d5d26282b5fe1ed27d09d5a7
yuktadandeva <153160647+yuktadandeva@users.noreply.github.com>
2024-07-09 00:52:27 +0530

    Update cart-service.js

--- PIZZA STORE/scripts/cart-service.js
+++ PIZZA STORE/scripts/cart-service.js
@@ -1,5 +1,18 @@
-const cartOperations = {addToCart(){
+const cartOperations = {
+pizzas : [],
 
+addToCart(pizzaId){
+
+    //iterate through pizzas and return pizzas with id 
+
+    // this.pizzas.find(function(currentPizza){return currentPizza.id == pizzaId })
+
+    //arrow-function
+    const pizza = this.pizzas.find(currentPizza => currentPizza.id == pizzaId);
+    pizza.isInCart = 'true'; 
+
+    //making a key value for pizzas that are added to the cart
+    console.log(this.pizzas);
 },
 
 removeFromCart(){
@@ -7,7 +20,7 @@ removeFromCart(){
 },
 
 viewAll(){
-
+    return this.pizzas.filter(pizza => pizza.isInCart);
 },
 
 totalPrice(){
